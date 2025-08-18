70caec994bd651c1e6c13c2b28ddb0a81c4f02bf
akslzero <aksyalgt@gmail.com>
2025-08-18 19:46:32 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use Filament\Facades\Filament;
+use Illuminate\Support\Facades\Artisan;
 
 
 Route::get('/', function () {
     return redirect('/admin/login');
+});
+
+Route::get('/clear-cache-temp', function () {
+    Artisan::call('optimize:clear');
+    return '✅ Cache cleared';
 });
